ab312997c4609cc65d38d8a03ce8a4542442293a
Neema Shokri <noshokri@uw.edu>
2022-02-28 21:25:35 -0800

    setup react native

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Test up App.js to start working on your app!</Text>
+      <Text>This is just a test</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
