66b28541d9a1c224d0abb9a8c88da3dd258a36e8
Romil Kinger <56062209+romilkinger@users.noreply.github.com>
2022-05-11 17:59:16 -0700

    added onboarding for new users

--- client/App.js
+++ client/App.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text, TextInput,TouchableOpacity } from 'react-native';
 import { MyTabs } from './navigation/MainContainer';
 import * as RequestManager from './utils/RequestManager';
 
 export default function App() {
 
   [initialized, setInit] = useState(false);
+  [userInfo, setUserInfo] = useState('');
 
   //Runs hook one time upon app initialization
   useEffect(() => {
@@ -14,7 +15,34 @@ export default function App() {
       .then(() => console.log('Current DeviceID:' + global.deviceId))
       .then(setInit(true));
   }, []);
-  return (initialized) ? <MyTabs /> : <View></View>;
+  return (initialized) ? <MyTabs /> : 
+  <View style={styles.container}>
+
+
+    <Text style={styles.paragraph}>
+      Welcome to Bump
+    </Text>
+
+
+    <Text style={styles.prompt}>
+      Tell us about yourself.
+    </Text>
+    
+    <TextInput
+      multiline
+      style={styles.input}
+      placeholder='e.g. I like netflix and coffee.'
+      onChangeText={(val) => setUserInfo(val)}
+    />
+
+    <TouchableOpacity
+      style = {styles.submitButton}
+      onPress = {
+        () => console.log(userInfo)
+      }>
+      <Text style = {styles.submitButtonText}> Submit </Text>
+    </TouchableOpacity>
+</View>;
 }
 
 const styles = StyleSheet.create({
@@ -24,5 +52,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  prompt: {
+    marginTop: 10,
+    marginBottom: 10,
+    fontSize: 12,
+  },
+  input: {
+    margin: 15,
+    borderColor: '#8664F6',
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 8,
+    height: 100
+  },
+  submitButton: {
+    backgroundColor: '#8664F6',
+    padding: 10,
+    marginVertical: 20,
+    height: 40,
+   },
+   submitButtonText:{
+      color: 'white'
+   }
 });
 
